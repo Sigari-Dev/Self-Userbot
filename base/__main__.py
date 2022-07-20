e16ec53fdec3622cfb178900b728b00cc20d92c8
mahdi <mahdidarksector@gmail.com>
2022-07-20 16:28:28 +0430

    Fixed api client / added a inlinequery handler

--- base/__main__.py
+++ base/__main__.py
@@ -2,6 +2,7 @@
 from . import BaseApi, BaseCli
 from pyrogram import compose
 import asyncio
+from config import bot_token
 
 async def main():
     apps = [
@@ -9,6 +10,5 @@ async def main():
         BaseApi(),
     ]
     await compose(apps)
-
 if __name__ == "__main__":
     asyncio.run(main())
--- base/pluginsApi/panel/panel.py
+++ base/pluginsApi/panel/panel.py
@@ -4,12 +4,13 @@ sys.path.insert(0, "...")
 from base import BaseApi
 from base.core.error_handler import _error
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineQuery
 from db import *
 from answers import answers
 import re
 from config import sudo_user
 
-@BaseApi.on_message(filters.user(sudo_user) & filters.command("start"))
-async def start(client: BaseApi, message: Message):
-    await message.reply("Nothing important here")
+@BaseApi.on_inline_query(filters.user(sudo_user))
+async def panel(client: BaseApi, update: InlineQuery):
+    # Handle inline query here
+    pass
