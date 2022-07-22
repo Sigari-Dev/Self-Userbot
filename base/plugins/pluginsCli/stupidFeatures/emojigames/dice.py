a32030bd11c1eed079edc98a6d9b81886a6f7671
CUMOON - Mahyar <95465931+CUMOON@users.noreply.github.com>
2022-07-23 01:10:17 +0430

    Add files via upload

--- base/plugins/pluginsCli/stupidFeatures/emojigames/dice.py
+++ base/plugins/pluginsCli/stupidFeatures/emojigames/dice.py
@@ -11,11 +11,18 @@ from base.core import Message, error
 from base.database import *
 from pyrogram import filters
 
-DART_EMOJI = ["🎯", "دارت", "dart"]
+DICE_EMOJI = ['🎲', 'تاس', 'dice']
 
-@BaseCli.on_message(filters.me & filters.regex(f'^({"|".join(DART_EMOJI)}) (6)$', re.I))
-@BaseCli.on_edited_message(filters.me & filters.regex(f'^({"|".join(DART_EMOJI)}) (6)$', re.I))
+@BaseCli.on_message(filters.me & filters.regex(f'^({"|".join(DICE_EMOJI)}) ([1-6])$', re.I))
+@BaseCli.on_edited_message(filters.me & filters.regex(f'^({"|".join(DICE_EMOJI)}) ([1-6])$', re.I))
 @error
-async def dart(client: BaseCli, message: Message):
-    #Later
-    pass
+async def dice(client: BaseCli, message: Message):
+    language = message.language()
+    required_number = message.text.split()[1]
+    await message.edit_text(answers['dice'][language].format(required_number))
+    while True:
+        msg = (await client.send_dice(message.chat.id, "🎲"))
+        if msg.dice.value != int(required_number):
+            await msg.delete()
+        else:
+            break
